939dc265642f5f9f333c56621947b3d59b08b96c
Davinder Sidhu <davinder@upgame.co>
2023-06-23 12:35:16 +0530

    update to 8.0.1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.upgame.common.Constants
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
